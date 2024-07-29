f41fd023669e81aa2545a0f36bd40f46fb610be5
Julien Grosthor <julien.grosthor@le-campus-numerique.fr>
2024-07-29 16:13:05 +0200

    modification du web.php

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
+});
+
+Route::get('/product', function () {
+    return '<h1>Liste des produits</h1>';
+});
+
+Route::get('/product/{id}', function ($id) {
+    return '<h1>Fiche du produit ' . $id . '</h1>';
+});
+
+Route::get('/cart', function () {
+    return '<h1>Panier</h1>';
 });
