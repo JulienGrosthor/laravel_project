9c81fc7613e966f3a9b8bbaa7d19ba26fbf1b49e
Julien Grosthor <julien.grosthor@le-campus-numerique.fr>
2024-08-05 10:25:32 +0200

    added BackofficeController, modified web.php...

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\BackofficeController;
 
 Route::get('/', [HomeController::class, 'home_index']);
 
@@ -12,3 +13,5 @@ Route::get('/product', [ProductController::class, 'product_index']);
 Route::get('/product/{id}', [ProductController::class, 'show'])->name('show.product');
 
 Route::get('/cart', [CartController::class, 'cart_index']);
+
+Route::get('/backoffice', [BackofficeController::class, 'backoffice_index']);
